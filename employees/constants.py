83fed33f9de3d6520ae89c2fa343f54f19667a57
MahmutAtia <104507838+MahmutAtia@users.noreply.github.com>
2022-04-30 00:41:50 +0300

    change pass

--- employees/constants.py
+++ employees/constants.py
@@ -1,2 +1,2 @@
-with open(r"C:\Users\user\Documents\data_scince\python_project\Excel_generator_v2\employees\dist.male.first", "r", encoding="UTF-8") as m:
+with open(r"employees/dist.male.first", "r", encoding="UTF-8") as m:
     male = m.read()  # converting to string
